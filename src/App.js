df63a629973b76194d6df952f0b6eb7651c244d6
Brandon <brandon.carter3@mavs.uta.edu>
2019-01-12 18:28:28 -0600

    Added Navigation and Movies components sections

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from "./header/Header";
+import Main from "./main/Main";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello World
+        <Header />
+        <Main />
       </div>
     );
   }
---
+++ src/main/movies/Movies.js
@@ -0,0 +1,16 @@
+// Movies.js
+
+import React from "react";
+import "./Movies.css";
+
+class Movies extends React.Component {
+    render() {
+        return (
+            <section className="movies">
+                Movies
+            </section>
+        )
+    }
+}
+
+export default Movies;
